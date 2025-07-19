ef056368fe5d122e60fa104532301feb964cd66c
rfarias2025 <robertojfarias@gmail.com>
2025-07-18 20:04:00 -0400

    Add files via upload

--- script_actualizar_inventario.py
+++ script_actualizar_inventario.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import json
 
-# Lee el archivo Excel (debe estar en la misma carpeta)
 df = pd.read_excel('inventario.xlsx')
 
-# Renombra las columnas para que coincidan con el index.html
 df_renombrado = df.rename(columns={
     'codigo': 'Código',
     'producto': 'Producto',
@@ -13,10 +11,8 @@ df_renombrado = df.rename(columns={
     'pvp': 'PVP ($)'
 })
 
-# Convierte a lista de diccionarios
 datos = df_renombrado.to_dict(orient='records')
 
-# Guarda en JSON con indentación bonita
 with open('inventario.json', 'w', encoding='utf-8') as f:
     json.dump(datos, f, ensure_ascii=False, indent=4)
 
